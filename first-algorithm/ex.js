1af900b8023a74ba681ff09d36ed178c49ae332d
HoeJun Kim <hjkim.it@gmail.com>
2021-05-25 09:11:40 +0900

    Add files via upload

---
+++ first-algorithm/ex.js
@@ -0,0 +1,25 @@
+const Triangle = (row, col) => {
+  if (col === 0) {
+    return 1;
+  } else if (row === 0) {
+    return 0;
+  } else {
+    return Triangle(row - 1, col) + Triangle(row - 1, col - 1);
+  }
+};
+
+let row = 5;
+let col = 1;
+
+const test = Array.from(new Array(row), () => {
+  return Array.from(new Array(col++).fill(0));
+});
+
+for (let i = 0; i < row; i++) {
+  test[i][0] = 1;
+  for (let j = 1; j <= i; j++) {
+    test[i][j] = Triangle(i, j);
+  }
+}
+
+console.log(test);
